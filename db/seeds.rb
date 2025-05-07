ff0cbdc36a8ff74fcc2039f6714cb8039c058acc
Manami Tsukada <manami.tsukada3@gmail.com>
2025-05-07 16:23:43 +0100

    added

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,20 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+puts 'Cleaning database...'
+Review.destroy_all
+Restaurant.destroy_all
+
+puts 'Creating restaurants...'
+
+dishoom = Restaurant.create!(name: 'Dishoom', address: '7 Boundary St, London E2 7JE', category: 'indian')
+puts 'Created Dishoom'
+pizza_east = Restaurant.create!(name: 'Pizza East', address: '56A Shoreditch High St, London E1 6PQ', category: 'italian')
+puts 'Created Pizza East'
+
+puts 'Creating reviews...'
+Review.create!(content: 'Amazing!', rating: 5, restaurant: dishoom)
+Review.create!(content: 'Good!', rating: 4, restaurant: dishoom)
+Review.create!(content: 'Great pizza', rating: 5, restaurant: pizza_east)
+
+puts "Finished! Created #{Restaurant.count} restaurants and #{Review.count} reviews."
